2adb4bc703fcf2aa8a241fd93c2bd93afeaf66e9
AQodS <qoddri@gmail.com>
2025-03-23 03:35:55 +0700

    feat: navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Navbar } from "@/components/ui/Navbar";
+import { navItems } from "@/data";
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto">
       <div className="max-w-7xl w-full">
+        <Navbar navItems={navItems} />
         <h1>TEST</h1>
       </div>
     </main>
